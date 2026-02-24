4f644fe67f693b4087d02f486fea4c87e1d84e18
Phillip Winder <phillip.winder@icloud.com>
2026-02-24 18:38:11 +0100

    init

--- docs-blitz-2/registry-reui/bases/radix/registry.ts
+++ docs-blitz-2/registry-reui/bases/radix/registry.ts
@@ -1,4 +1,5 @@
 import { registryItemSchema, type Registry } from "shadcn/schema"
+import { z } from "zod"
 
 import { hooks } from "./hooks/_registry"
 import { patterns } from "./patterns/_registry"
@@ -7,5 +8,5 @@ import { reui } from "./reui/_registry"
 export const registry = {
   name: "shadcn/ui",
   homepage: "https://ui.shadcn.com",
-  items: registryItemSchema.array().parse([...reui, ...hooks, ...patterns]),
+  items: z.array(registryItemSchema).parse([...reui, ...hooks, ...patterns]),
 } satisfies Registry
