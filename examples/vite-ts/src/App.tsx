8d66e677baae8bace31871387ed0f92f8eb36144
Phillip Winder <phillip.winder@icloud.com>
2026-03-08 21:18:53 +0100

    init

--- examples/vite-ts/src/App.tsx
+++ examples/vite-ts/src/App.tsx
@@ -3,13 +3,12 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import { Button } from 'blitz-ui/button';
-import { Skeleton } from 'blitz-ui/skeleton';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="style-mira">
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -20,14 +19,15 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
-        <Skeleton />
+        <Button variant="default" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    </div>
   );
 }
 
